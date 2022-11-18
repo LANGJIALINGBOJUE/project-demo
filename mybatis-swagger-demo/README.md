0c19baab847e016d433d6047e219713f61fcc2bc
LANGJIALINGBOJUE <1830879596@qq.com>
2022-11-18 17:36:31 +0800

    mybatis整合swagger项目Demo

---
+++ mybatis-swagger-demo/README.md
@@ -0,0 +1,3 @@
+# 项目功能
+
+此项目使用 `SpringBoot` 整合 `Swagger` 和 `Knife4j` 。
---
+++ mybatis-swagger-demo/src/main/java/com/example/mybatisdemo0/MybatisDemo0Application.java
@@ -0,0 +1,13 @@
+package com.example.mybatisdemo0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDemo0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDemo0Application.class, args);
+    }
+
+}
